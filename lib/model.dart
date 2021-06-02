09c0367c19020f8994ee338ee3ce74cfe3bf918e
XuJiaAng <xujiaang@bytedance.com>
2021-06-02 20:04:02 +0800

    搜索算法

--- lib/model.dart
+++ lib/model.dart
@@ -1,18 +1,86 @@
+import 'dart:async';
 import 'dart:io';
 
 class Novel {
   Novel(this.file);
 
   File file;
-  String?  _cache ;
-  String _getCache() {
-    if (_cache != null) {
-      _cache = file.readAsStringSync();
+  String? _cache;
+
+  FutureOr<String> _getCache() async {
+    if (_cache == null) {
+      // final data = await file.readAsBytes();
+      // _cache = utf8.decode(data.sublist(3));
+      _cache = await file.readAsString();
     }
     return _cache!;
   }
-  List<String> search(String key) {
-    final cache = _getCache();
-    return cache.allMatches(key).map((e) => cache.substring(e.start,e.end)).toList();
+
+  FutureOr<List<String>> search(String key) async {
+    final data = await _getCache();
+    print(data);
+    final re = data.search(key, 20);
+    print("re.length ${re.length}");
+    return re;
+  }
+}
+
+extension seach on String {
+  List<String> search(String key, int targetLength) {
+    final matches = key.allMatches(this);
+    print("matches ${matches.length}");
+    final list = matches.toList();
+    if (list.isEmpty) {
+      return [];
+    } else {
+      final eaten = list.iterator;
+      eaten.moveNext();
+      final finish = [[eaten.current.start,eaten.current.end]];
+      while (eaten.moveNext()) {
+        var needEat = false;
+        if (eaten.current.end - finish.last.first <= targetLength) {
+          needEat = true;
+          for (int i = finish.last.first; i < eaten.current.end; ++i) {
+            if (this[i] == '\n') {
+              needEat = false;
+              break;
+            }
+          }
+        }
+        if (needEat) {
+          finish.last.last = eaten.current.end;
+        } else {
+          finish.add([eaten.current.start,eaten.current.end]);
+        }
+      }
+      return finish.map((e) => cut(e.first,e.last,targetLength)).toList();
+    }
+  }
+
+  String cut(int start, int end, int targetLength) {
+    var tryStartFirst = true,
+        changed = true;
+    while (end - start < targetLength && changed) {
+      tryStartFirst = !tryStartFirst;
+      if (tryStartFirst) {
+        if (start > 0 && this[start - 1] != '\n') {
+          --start;
+        } else if ((end < length && this[end] != '\n') ||
+            end == length) {
+          ++end;
+        } else {
+          changed = false;
+        }
+      } else {
+        if ((end < length && this[end] != '\n') || end == length) {
+          ++end;
+        } else if (start > 0 && this[start - 1] != '\n') {
+          --start;
+        } else {
+          changed = false;
+        }
+      }
+    }
+    return substring(start, end);
   }
 }
