b65ffa67041a5e59b6ddab5f25fda6fd53db0a41
Miliapus <xujiaang@bytedance.com>
2021-06-10 19:36:08 +0800

    主页剥离

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:novel_text_searcher/manager.dart';
-import 'package:novel_text_searcher/model.dart';
+import 'main_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,54 +17,5 @@ class MyApp extends StatelessWidget {
       ),
       home: MyHomePage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> implements NovelManagerDelegate {
-  List<String> novels= [];
-  final novelManager = NovelManager();
-  final searchInputController = TextEditingController();
-  @override
-  void initState() {
-    super.initState();
-    novelManager.delegate = this;
-    novelManager.load();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: TextField(
-          controller: searchInputController,
-          onSubmitted: (text){
-            novels.clear();
-            novelManager.search(text);
-          },
-        ),
-      ),
-      body: Center(
-        child: ListView(
-          children: novels.map((e) => ListTile(
-            subtitle: Text(e),
-          )).toList(),
-        )
-      ),
-    );
-  }
-
-  @override
-  void onOnceSearchFinish(Novel novel, List<String> results) {
-    // TODO: implement onOnceSearchFinish
-    print("${results.length} results.length");
-    setState(() {
-      novels.addAll(results);
-    });
   }
 }
