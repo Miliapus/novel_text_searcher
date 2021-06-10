34e8b7ccb431f8f1cb636c7e4dee042414e2e950
Miliapus <xujiaang@bytedance.com>
2021-06-10 19:36:56 +0800

    完全搜索完毕再更新ui

--- lib/manager.dart
+++ lib/manager.dart
@@ -1,15 +1,16 @@
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:novel_text_searcher/model.dart';
 
 abstract class NovelManagerDelegate {
   void onOnceSearchFinish(Novel novel, List<String> results);
+  void onAllSearchFinish();
 }
 
 class NovelManager {
   NovelManagerDelegate? delegate;
   List<Novel> novels = [];
+  int searchFinish = 0;
 
   load() async {
     Directory dir = Directory("/storage/emulated/0/小说/其他小说");
@@ -20,13 +21,21 @@ class NovelManager {
   }
 
   search(String key) async {
+    searchFinish = 0;
     novels.forEach((element) {
       onceSearch(element, key);
     });
   }
 
   onceSearch(Novel novel, String key) async {
-    final results = await novel.search(key);
-    delegate!.onOnceSearchFinish(novel, results);
+    try {
+      final results = await novel.search(key);
+      delegate!.onOnceSearchFinish(novel, results);
+    } finally {
+      searchFinish +=1;
+      if (searchFinish == novels.length) {
+        delegate!.onAllSearchFinish();
+      }
+    }
   }
 }
